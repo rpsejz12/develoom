326f881a6074df2844a9ec66a834f3d42dd5cec1
rpsejz12 <rpsejz12@naver.com>
2021-10-24 22:49:05 +0900

    실패하였다..

---
+++ develoom/src/main/java/com/kim/app/ControllerView.java
@@ -0,0 +1,18 @@
+package com.kim.app;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ControllerView {
+	// 채팅방 입장
+	@RequestMapping(value = "/app/chat.do", method = RequestMethod.GET)
+	public String view_chat(HttpServletRequest request, HttpServletResponse response, Model model) throws Exception {
+		return "chat";
+	}
+}
